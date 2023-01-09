2de07f5606a36a3c06df6fd0f9da22694ec1581c
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2023-01-10 01:10:15 +0600

    anoteer update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.min';
 import Footer from './Pages/Shared/Footer/Footer';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Footer/>
     </div>
   );
