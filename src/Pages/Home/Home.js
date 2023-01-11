672ba83586995208a868bf2510637904244fb163
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2023-01-11 17:37:51 +0600

    header first part update

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div style={{height:'700px', backgroundColor:'skyblue',/*  marginTop:'94px' */}}>
+        <div style={{height:'700px', backgroundColor:'',/*  marginTop:'94px' */}}>
             <h1>Home</h1>
         </div>
     );
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import logo from '../../../img/mahsez.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeadset } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
+    const [shadow, setShadow] = useState(false)
+    const changeShadow = () =>{
+        if(window.scrollY >= 50){
+            setShadow(true)
+        }
+        else{
+            setShadow(false)
+        }
+    }
+    window.addEventListener('scroll', changeShadow)
+
+
     return (
-        <div>
+        <>
             {/* ---------Part 1 ----------- */}
           
-            <div style={{ padding: '20px 0px' }}>
+            <div className={shadow ? 'sticky-top  header-shadow': 'sticky-top'} style={{ padding: '20px 0px' }}>
                 <div className='container  d-flex justify-content-between align-items-center'>
                     <div>
                         <img width='150px' src={logo} alt='' />
@@ -45,7 +57,7 @@ const Header = () => {
 
 
             {/* ---------Part 2 ----------- */}
-        </div>
+        </>
     );
 };
 
