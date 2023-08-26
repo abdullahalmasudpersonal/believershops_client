585bd7f384d4ee22d097fc37b7e35949c8fb2042
Abdullah Al Masud <abdullahalmasud01737906772@gmail.com>
2023-08-26 19:49:01 +0600

    update porductsByKeys

--- src/Hooks/UseCarts/UseCart.js
+++ src/Hooks/UseCarts/UseCart.js
@@ -9,34 +9,7 @@ const UseCart = () => {
         const savedCart = [];
         const keys = Object.keys(storedCart);
 
-        /*       async function init() {
-                  try {
-                      const results = await Promise.all([
-                          fetch('http://localhost:5000/attarsByKeys').then(response => response.json()),
-                          fetch('http://localhost:5000/capsByKeys').then(response => response.json())
-                      ].map(promise => promise.catch(error => error)));
-                      console.log('masud',results);
-                  } catch (error) {
-                      console.log(error)
-                  }
-              }
-              init(); */
-
-        /*    const url1 = fetch('http://localhost:5000/attarsByKeys');
-           const url2 = fetch('http://localhost:5000/attarsByKeys');
-           Promise.all([url1, url2]).then(values =>{
-               console.log('masuds',values)
-               return Promise.all(values.map(r => r.json()));
-           }).then(values => {
-               console.log(values)
-           });
-    */
-        /*   const url = [
-              'http://localhost:5000/attarsByKeys',
-              'http://localhost:5000/capsByKeys',
-          ]; */
-
-        fetch('http://localhost:5000/productsByKeys', {
+        fetch('http://localhost:5000/api/v1/products/productsByKeys', {
             method: 'POST',
             headers: {
                 'content-type': 'application/json'
